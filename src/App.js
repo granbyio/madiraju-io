992ff0af944be4dfe6e2d83cf7fa7c81ebe19baa
Sandilya <gitmanmadi@gmail.com>
2025-02-27 17:11:18 -0600

    portfolio website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="hero">
+      <h1 className="hero-title">Sandilya Madiraju.</h1>
+      <p className="hero-subtitle">
+        product, technology &amp; 
+        <span className="dynamic-text">
+        <Typewriter
+            words={[' strategy', ' growth', ' design', ' business', ' innovation', ' leadership', ]}
+            loop={true}
+            typeSpeed={40}
+            deleteSpeed={100}
+            delaySpeed={3000}
+          />
+        </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+      <div className="hero-icons">
+
+        <a href='https://github.com/granbyio' target='_blank' rel='noopener noreferrer' aria-label='GitHub'>
+          <ion-icon name="logo-github"></ion-icon>
         </a>
-      </header>
+
+        <a href='https://www.linkedin.com/in/sandilyamadiraju/' target='_blank' rel='noopener noreferrer' aria-label='GitHub'>
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a>
+
+        <a href='https://medium.com/@granbyio' target='_blank' rel='noopener noreferrer' aria-label='Medium'>
+          <ion-icon name="document-outline"></ion-icon>
+        </a>
+
+        <a href='mailto:sandi@madiraju.io' target='_blank' rel='noopener noreferrer' aria-label='Contact'>
+          <ion-icon name="mail-outline"></ion-icon>
+        </a>
+
+      </div>
     </div>
   );
 }
