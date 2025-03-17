7102104b5600343f198dc51a3a03182f3f8b79a8
granbyio <sandi@madiraju.io>
2025-03-17 18:01:34 -0500

    setup seo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Typewriter } from 'react-simple-typewriter';
+import { Helmet } from 'react-helmet-async';
 
 function App() {
   return (
+    <>
+    {/* Helmet for SEO Metadata */}
+    <Helmet>
+      <title> Sandilya Madiraju Personal Portfolio </title>
+      <meta name="description" content="Sandilya Madiraju Personal Portfolio. Github, LinkedIn, Blog, and Email!" />
+      <meta name="keywords" content="sandilya, madiraju, sandi, product management, technology, innovation, leadership, growth, strategy" />
+      <meta name="robots" content="index, follow" />
+    </Helmet>
+
+    {/* Main Website Content */}
     <div className="hero">
       <h1 className="hero-title">Sandilya Madiraju.</h1>
       <p className="hero-subtitle">
@@ -39,6 +50,7 @@ function App() {
 
       </div>
     </div>
+    </>
   );
 }
 
