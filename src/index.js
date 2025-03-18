f960f0602f1ce95140c886c471b4fe367c2fd2a4
granbyio <sandi@madiraju.io>
2025-03-17 21:11:43 -0500

    Fix: Added HelmetProvider to resolve blank screen issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -29,7 +30,9 @@ addGoogleAnalytics();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
